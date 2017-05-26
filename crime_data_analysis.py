66aaabebcc93c4b78b3f33b90b26d05d53a5f54f
jastr945 <polina.jastr@gmail.com>
2017-05-26 13:52:21 -0700

    Finished lab.

--- crime_data_analysis.py
+++ crime_data_analysis.py
@@ -16,26 +16,6 @@ Print out a summary of the data:
 
 import csv
 
-# with open('/home/polina/projects/class/crime_incident_data_2011.csv','r') as text:
-#
-#     original_dict = csv.DictReader(text)
-#     # for line in original_dict:
-#     #     print(line['Major Offense Type'])
-#
-#     new_dict = {}
-#
-#     for line in original_dict:
-#         if line['Major Offense Type'] in new_dict:
-#             new_dict[line['Major Offense Type']] += 1
-#         else:
-#             new_dict[line['Major Offense Type']] = 1
-#
-#     text_return = ""
-#     for k, v in new_dict.items():
-#         text_return += '{}: {}\n'.format(k,v)
-#     print(text_return)
-
-
 def count_crime(crime_incident_data, new_dict={}):
     with open(crime_incident_data, 'r') as text:
 
@@ -52,13 +32,14 @@ def count_crime(crime_incident_data, new_dict={}):
 
 year_2011 = count_crime('/home/polina/projects/class/crime_incident_data_2011.csv')
 
-year_2012 = count_crime('/home/polina/projects/class/crime_incident_data_2012.csv', year_2011)
+year_2012 = count_crime('/home/polina/projects/class/crime_incident_data_2012.csv',year_2011)
 
-year_2013 = count_crime('/home/polina/projects/class/crime_incident_data_2013.csv', year_2012)
 
-year_2014 = count_crime('/home/polina/projects/class/crime_incident_data_2014.csv', year_2013)
+year_2013 = count_crime('/home/polina/projects/class/crime_incident_data_2013.csv',year_2012)
 
-recent_data = count_crime('/home/polina/projects/class/crime_incident_data_recent.csv', year_2014)
+year_2014 = count_crime('/home/polina/projects/class/crime_incident_data_2014.csv',year_2013)
+
+recent_data = count_crime('/home/polina/projects/class/crime_incident_data_recent.csv',year_2014)
 
 
 for k, v in recent_data.items():
@@ -68,5 +49,16 @@ for k, v in recent_data.items():
 for k, v in recent_data.items():
     if v == min(recent_data.values()):
         print('The rarest type of crime is: {}. Number of cases: {}.'.format(k,v))
-print(recent_data)
 
+
+year_summary = {
+    'year 2011': sum(count_crime('/home/polina/projects/class/crime_incident_data_2011.csv').values()),
+    'year 2012': sum(count_crime('/home/polina/projects/class/crime_incident_data_2012.csv').values()),
+    'year 2013': sum(count_crime('/home/polina/projects/class/crime_incident_data_2013.csv').values()),
+    'year 2014': sum(count_crime('/home/polina/projects/class/crime_incident_data_2014.csv').values()),
+    'recent year': sum(count_crime('/home/polina/projects/class/crime_incident_data_recent.csv').values())
+}
+
+for k, v in year_summary.items():
+    if v == max(year_summary.values()):
+        print('The year with most crime is: {}; the total number of crimes committed during this period is: {}.'.format(k,(max(year_summary.values()))))
