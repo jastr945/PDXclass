409f0a157463a697a20f14b7d22e9213ba439e9d
jastr945 <polina.jastr@gmail.com>
2017-06-01 12:04:27 -0700

    Printed the deck for the blackjack game.

---
+++ blackjack.py
@@ -0,0 +1,77 @@
+'''
+
+Let's start modeling a game of blackjack.
+
+Card class with a suit and a rank Deck class with a collection of cards Hand class with a collection of cards from a deck.
+
+Use multiple modules.
+
+In the deck module, implement methods that:
+
+    Return a shuffled deck
+    Cut the deck
+    Draw a card off the top of a deck
+
+In the hand module, implement methods that:
+
+    Add a card to a hand
+    Allow a user to type in a hand and have it be converted into a Hand object
+
+In the game module, implement top-level functions that:
+
+    Start a new game of Blackjack, or Quit
+    Score a hand
+    Bust if the score is over 21
+
+
+'''
+
+
+
+# from random import randin
+# from random import shuffle
+
+
+suits = ['Clubs', 'Diamonds', 'Hearts', 'Spades']
+ranks = ['Ace', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'King', 'Queen']
+
+
+class Card:
+    """
+    This class creates a card.
+    """
+
+
+    def __init__(self, rank, suit):
+        self.rank = rank
+        self.suit = suit
+
+
+    def __repr__(self):
+        return '{} of {}'. format(self.rank, self.suit)
+
+
+class Deck:
+    """
+    This class creates an entire deck.
+    """
+
+
+    def __init__(self):
+        self.cards = []
+        for suit in suits:
+            for rank in ranks:
+                self.cards.append(Card(rank,suit))
+
+
+    def __repr__(self):
+        """
+        Returns a string containing a printable representation of an object.
+        """
+        return '{}'.format(self.rank, self.suit)
+
+
+d = Deck()
+print(d.cards)
+
+
