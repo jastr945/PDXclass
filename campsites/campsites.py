86425c942078c0abc8a0109175eb9b85504e9d7d
jastr945 <polina.jastr@gmail.com>
2017-06-15 16:18:24 -0700

    Developed the Camground class,inheritance and a test for it.

--- campsites/campsites.py
+++ campsites/campsites.py
@@ -1,25 +1,14 @@
 """
 'FIND A CAMPGROUND'
 
-This program will allow users to search for a limited number of the most famous campgrounds in Oregon, depending on
-keywords (for example, if a user types in "Woodburn", the program will display the profile of the Woodburn RV Park).
-The program will take care of invalid user input, too. Each campsite profile will contain basic information, such as
-location, number of spaces (capacity), facilities, ect. The program will download weather data and display the current weather
-for the area where each campsite is located.
+This program will allow users to search for a limited number of the most famous campgrounds in Oregon by a keyword
+(for example, if a user enters "Woodburn", the program will display the profile of the Woodburn RV Park).
 
+Each campsite profile will contain basic information, such as location, number of spaces (capacity), facilities available, ect.
 
-* If a profile is missing, the user can create it! (add this feature if I have time)
+The program downloads weather data and displays the current weather for the area where each campsite is located.
 
-The profile will contain the list of facilities / amenities:
-* Free Parking
-* Free High Speed Internet ( WiFi )
-* flush toilets / pit toilets
-* showers
-* pool
-* pets allowed / pets not allowed (true/ false kind of choice)
-* kid / family friendly (true / false kind of choice)
-* for RV: water and sewer hook-ups.
-* for tents: picnic areas
+If a campground name is missing in the database, the user is able to create a new record and insert it into the database.
 
 
 Example Output:
@@ -113,37 +102,30 @@ class DatabaseManager(object):
 
         self.conn.commit()
 
-    def delete(self, id):
-        """
-        Deletes a row in the table according to its ID (primary key).
-        """
-        self.conn.execute("DELETE FROM CAMPGROUNDS WHERE ID={}".format(id))
-        print('Record deleted successfully.')
-
     def get_all(self):
         """
         Returns the data from the table in a form of a dictionary.
         """
         cursor = self.conn.execute("SELECT * FROM CAMPGROUNDS")
-        columns = [i[0] for i in cursor.description]
-        return [dict(zip(columns, i)) for i in cursor.fetchall()]
+        columns = [i[0] for i in cursor.description]            # .description method returns the names of all columns
+        return [dict(zip(columns, i)) for i in cursor.fetchall()]  # .fetchall method fetches all rows of a query result set and returns a list of tuples. Zip function matches the two tuples into one. Dict makes a dictionary of of it.
 
     def get(self, id):
         """
         Returns a row from the table depending on ID (primary key).
         """
         cursor = self.conn.execute("SELECT * FROM CAMPGROUNDS WHERE ID={}".format(id))
-        columns = [i[0] for i in cursor.description]
-        return [dict(zip(columns, i)) for i in cursor.fetchall()]
+        columns = [i[0] for i in cursor.description]            # .description method returns the names of all columns
+        return [dict(zip(columns, i)) for i in cursor.fetchall()]  # .fetchall method fetches all rows of a query result set and returns a list of tuples. Zip function matches the two tuples into one. Dict makes a dictionary of of it.
 
     def search(self):
         """
         Looks for the name enter by the user and returns the row containing the name.
         """
-        mystr = str(input('Enter the name of a campground: ')).capitalize()
+        mystr = str(input('Enter the name of a campground you want to find: ')).capitalize()
         cursor = self.conn.execute("SELECT * FROM CAMPGROUNDS WHERE NAME LIKE ?", ('%'+mystr+'%',))
-        columns = [i[0] for i in cursor.description]
-        return [dict(zip(columns, i)) for i in cursor.fetchall()]
+        columns = [i[0] for i in cursor.description]            # .description method returns the names of all columns
+        return [dict(zip(columns, i)) for i in cursor.fetchall()]  # # .fetchall method fetches all rows of a query result set and returns a list of tuples. Zip function matches the two tuples into one. Dict makes a dictionary of of it.
 
     def create(self):
         """
@@ -167,38 +149,49 @@ class DatabaseManager(object):
         new_data = (new_name,new_type,new_location,new_capacity,new_parking,new_internet,new_restrooms,new_showers,new_pool,new_pet_friendly,new_family_friendly,new_water_hook_up,new_sewer_hook_up,new_picnic_area)
 
         self.conn.execute("INSERT INTO CAMPGROUNDS (NAME,TYPE,LOCATION,CAPACITY,PARKING,INTERNET,RESTROOMS,SHOWERS,POOL,PET_FRIENDLY,FAMILY_FRIENDLY,WATER_HOOK_UP, SEWER_HOOK_UP,PICNIC_AREA) \
-                              VALUES (11, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",(new_data,));
+                              VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", (new_data,));
 
         self.conn.commit()
 
-        print('Your record was successfully saved in te database.')
+        print('Your record was successfully saved in the database.')
+
+    def delete(self, id):
+        """
+        Deletes a row in the table according to its ID (primary key).
+        """
+        self.conn.execute("DELETE FROM CAMPGROUNDS WHERE ID={}".format(id))
+        print('Record deleted successfully.')
 
 
-class Campground(DatabaseManager):
+class Campground:
     """
     Contains a prototype of every campground and inherits functionality of the DatabaseManager class.
     """
-    def __init__(self, name, type, location, capacity):
+    def __init__(self, name, camptype, location, capacity, parking, internet, restrooms, showers, pool, pets, family):
+        # super(DatabaseManager, self).__init__(":memory:")
         self.name = name
-        self.type = type
+        self.camptype = camptype
         self.location = location
         self.capacity = capacity
-        self.has_parking = has_parking
-        self.has_internet = has_internet
-        self.toilet = toilet
-        self.has_showers = has_showers
-        self.has_pool = has_pool
-        self.pet_friendly = pet_friendly
-        self.family_friendly = family_friendly
+        self.parking = parking
+        self.internet = internet
+        self.restrooms = restrooms
+        self.showers = showers
+        self.pool = pool
+        self.pets = pets
+        self.family = family
+
+    def __repr__(self):
+        return 'Campground: {}\nType: {}\nLocation: {}\nCapacity: {}\nParking: {}\nInternet: {}\nToilet: {}\nShowers: ' \
+               '{}\nPool: {}\nPet-friendly: {}\nFamily-friendly: {}'.format(self.name,self.camptype,self.location,self.capacity,self.parking,self.internet,self.restrooms,self.showers,self.pool,self.pets,self.family)
 
 
 class RV(Campground, DatabaseManager):
     """
     Adds whether there is a sewer and water hook-up available in RV parks.
     """
-    def __init__(self, name, water, sewer):
-        super(Campground, self).__init__(name)
-        super(DatabaseManager, self).__init__(":memory:")
+    def __init__(self, water, sewer):
+        super(Campground, self).__init__()
         self.water = water
         self.sewer = sewer
 
@@ -216,6 +209,10 @@ class Tentsite(Campground, DatabaseManager):
 # Works with APIs to dowload the current weather reports for each location.
 
 mydb = DatabaseManager('campgrounds.db')
+print('Welcome to the Oregon Campgrounds Database!')
+print(Campground(name='blabla',camptype='Camp',location='portland',capacity=6,parking='yes',internet='yes',restrooms='yes',showers='yes',pool='no',pets='no',family='yes'))
 #print(mydb.syncdb())
 # print(mydb.create())
-print(mydb.get_all())
+# print(mydb.get_all())
+
+
