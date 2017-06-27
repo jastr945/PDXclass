9dcca44c231132628ab0e0949491ac91c3b753fe
jastr945 <polina.jastr@gmail.com>
2017-06-27 16:04:01 -0700

    Ajax weather lab without pictures

---
+++ JavaScript/weather/weather.js
@@ -0,0 +1,18 @@
+function getWeather () {
+    $.ajax({
+        url: "http://api.openweathermap.org/data/2.5/weather",
+        data: {
+            APPID: '6b919e5d511686a6a70d2728794a6fe5',
+            q: $('#city').val()
+        },
+        dataType: "jsonp",
+        type: "post",
+        success: function (data) {
+            $('#result').append('Weather: ' + data['weather'][0]['description'] + '<br/>' + 'Temperature: ' + data['main']['temp'] + '<br/>' +
+                'Wind: ' + data['wind']['speed'] + '<br/>' + 'Humidity: ' + data['main']['humidity']
+            + '<br/>' + 'Pressure: ' + data['main']['pressure']);
+        }
+    });
+}
+
+$('#checkWeather').click(getWeather);
