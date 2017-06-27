c1b597029c656058719fbb360464f69621b8ef23
jastr945 <polina.jastr@gmail.com>
2017-06-27 16:32:47 -0700

    Added a function for inserting pics

--- JavaScript/weather/weather.js
+++ JavaScript/weather/weather.js
@@ -1,3 +1,11 @@
+function insertPic () {
+    if (data['weather']['id'] === '500') {
+        $('.content').css('background-image', 'url(rain.jpg)');
+    } else if (data['weather']['id'] === '500') {
+        $('.content').css('background-image', 'url(sun.jpg)')
+    }
+}
+
 function getWeather () {
     $.ajax({
         url: "http://api.openweathermap.org/data/2.5/weather",
@@ -11,8 +19,10 @@ function getWeather () {
             $('#result').append('Weather: ' + data['weather'][0]['description'] + '<br/>' + 'Temperature: ' + data['main']['temp'] + '<br/>' +
                 'Wind: ' + data['wind']['speed'] + '<br/>' + 'Humidity: ' + data['main']['humidity']
             + '<br/>' + 'Pressure: ' + data['main']['pressure']);
-        }
-    });
+            }
+
+        });
 }
+
 
 $('#checkWeather').click(getWeather);
