24ed609f24f23b199dd7214d35ee061002c901dd
jastr945 <polina.jastr@gmail.com>
2017-06-27 23:05:06 -0700

    Finished task. Will work on design later

--- JavaScript/weather/weather.js
+++ JavaScript/weather/weather.js
@@ -1,8 +1,18 @@
-function insertPic () {
-    if (data['weather']['id'] === '500') {
-        $('.content').css('background-image', 'url(rain.jpg)');
-    } else if (data['weather']['id'] === '500') {
-        $('.content').css('background-image', 'url(sun.jpg)')
+function insertPic (someid) {
+    if (someid >= 200 && someid < 300) {
+        $('.content').css({'background-image': 'url(thunderstorm.jpg)'});
+    } else if (someid >= 300 && someid <= 531) {
+        $('.content').css({'background-image': 'url(rain.jpg)'});
+    } else if (someid >= 600 && someid <= 622) {
+        $('.content').css({'background-image': 'url(snow.jpeg)'});
+    } else if (someid >= 701 && someid <= 781) {
+        $('.content').css({'background-image': 'url(fog.jpg)'});
+    } else if (someid === '800') {
+        $('.content').css('background-image', 'url(sun.jpg)');
+    } else if (someid >= 801 && someid <= 804) {
+        $('.content').css('background-image', 'url(clouds.jpg)');
+    } else if (someid >= 900 && someid <= 962) {
+        $('.content').css('background-image', 'url(warning.jpg)');
     }
 }
 
@@ -19,10 +29,11 @@ function getWeather () {
             $('#result').append('Weather: ' + data['weather'][0]['description'] + '<br/>' + 'Temperature: ' + data['main']['temp'] + '<br/>' +
                 'Wind: ' + data['wind']['speed'] + '<br/>' + 'Humidity: ' + data['main']['humidity']
             + '<br/>' + 'Pressure: ' + data['main']['pressure']);
+            var weatherId = data['weather'][0]['id'].toString();
+            insertPic(weatherId)
             }
 
         });
 }
-
 
 $('#checkWeather').click(getWeather);
