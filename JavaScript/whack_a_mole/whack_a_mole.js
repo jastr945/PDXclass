de825e206a67f9021156d28ccced5726bbf81628
jastr945 <polina.jastr@gmail.com>
2017-06-30 14:43:52 -0700

    Score only counts when a mole is on. Added timer.

--- JavaScript/whack_a_mole/whack_a_mole.js
+++ JavaScript/whack_a_mole/whack_a_mole.js
@@ -1,22 +1,30 @@
-var score  = 0;
+var score = 0;
+var t = 0;
+var myTimer;
 var myInterval; //making this variable available for other functions
 
 
 //uses a random number to inject a picture of a mole into a div with a corresponding id number
 function pickHole () {
     var holeNumber = (Math.floor(Math.random() * 20) + 1);
-    $('#' + holeNumber).html("<img src='mole.jpg'/>");
+    $('#' + holeNumber).html("<img src='mole.jpg'/>").addClass('active');
 
     //changes image back to a hole and adds score points ONLY when the image on a mole is on
     $('#' + holeNumber).click(function () {
-    $('#' + holeNumber).html("<img src='hole.jpg'/>");
-    score++;
-    $('#timer').html(score);
-});
+        if ($(this).attr('class') === 'active') {
+            $(this).html("<img src='hole.jpg'/>").removeClass('active');
+            score++;
+            $('#scoreCounter').html(score);
+        }
+    });
 }
 
-//makes moles appear every second
+//makes moles appear every second and starts counting the time upon clicking the 'start game' button
 $('#start').click(function () {
+    myTimer = setInterval(function () {
+        $('#timeCounter').html(t);
+        t++;},1000);
+
     myInterval = setInterval(pickHole, 1000);
     pickHole();
 });
@@ -37,7 +45,8 @@ $('#stop').click(function () {
 // sets score to none, stops the counter and resets all images to holes
 $('#reset').click(function () {
     clearInterval(myInterval);
-    $('#timer').html(0);
+    clearInterval(myTimer);
+    $('#scoreCounter').html(0);
     $( "#row1" ).find('*').html("<img src='hole.jpg'/>");
     $( "#row2" ).find('*').html("<img src='hole.jpg'/>");
     $( "#row3" ).find('*').html("<img src='hole.jpg'/>");
