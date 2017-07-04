62a4ffc4f858075f34a22ce454ab731c40ca3fa2
jastr945 <polina.jastr@gmail.com>
2017-07-03 23:02:23 -0700

    Fully working encrypt function

--- JavaScript/ceasar/site.js
+++ JavaScript/ceasar/site.js
@@ -1,34 +1,54 @@
 'use strict';
 
 var alphabet = ['a','b','c','d','e','f','j','h','i','g','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+var newIndex;
+var newLetter;
 
 //Iterates over a given string; matches each letter with the alphabet array; gets its index and calculates the new index,
 //based on the given key; finds a new letter and returns a new string.
 function caesarEncrypt(plainStr, key) {
     var result = '';
-    for(var i = 0, len = plainStr.length; i < len; i++) {
-        var indexInAlphabet = alphabet.indexOf(plainStr[i]);
-        var newIndex = indexInAlphabet + parseInt(key);
-        var newLetter = alphabet[newIndex];
-        result += newLetter;
+    for (var i = 0, len = plainStr.length; i < len; i++) {
+        var indexInAlphabet = alphabet.indexOf(plainStr[i]); // determining the index of a letter in the alphabet array
+        var alphabetRemainder = alphabet.length - (indexInAlphabet + 1); // determining how many letters are left till the end of array
+
+        if (parseInt(key) <= alphabetRemainder) {
+            newIndex = indexInAlphabet + parseInt(key); // determining a new index
+            newLetter = alphabet[newIndex]; // determining a new letter
+            result += newLetter;
+        } else {
+            //goes though the alphabet array several times until the right index is found
+            if ((parseInt(key) + alphabetRemainder) > alphabet.length) {
+                if ((parseInt(key) - alphabetRemainder) % alphabet.length === 0) {
+                    newLetter = alphabet[alphabet.length - 1];
+                    result += newLetter;
+                } else {
+                    newIndex = ((parseInt(key) - alphabetRemainder) % alphabet.length) - 1;
+                    newLetter = alphabet[newIndex];
+                    result += newLetter;
+                }
+            } else if ((parseInt(key) + alphabetRemainder) === alphabet.length) {
+                newLetter = alphabet[alphabet.length - 1];
+                result += newLetter;
+            } else {
+                newIndex = (parseInt(key) - alphabetRemainder) - 1;
+                newLetter = alphabet[newIndex];
+                result += newLetter;
+            }
+        }
+        $('#encryptResult').html(result);
     }
-    document.getElementById('encryptResult').innerHTML = result;
 }
-
-// If  key > (alphabet.length - indexInAlphabet):
-// newIndex = key - (alphabet.length - indexInAlphabet) - 1
-
-// Connects the encrypting function to the text window and submit button
-document.getElementById('encrypt').addEventListener('click', function (e) {
+// encrypts upon clicking the 'submit' button
+$('#encrypt').click(function (e) {
     e.preventDefault();
-    var EncryptStr = document.getElementById('encryptString').value;
-    var EncryptKey = document.getElementById('encryptKey').value;
-    caesarEncrypt(EncryptStr, EncryptKey);
+    caesarEncrypt($('#encryptString').val(), $('#encryptKey').val());
 });
 
+//an opposite process with a similar logic as above
 function caesarDecrypt(encStr, key) {
     var result = '';
-    for(var i = 0, len = encStr.length; i < len; i++) {
+    for (var i = 0, len = encStr.length; i < len; i++) {
         var indexInAlphabet = alphabet.indexOf(encStr[i]);
         var newIndex = indexInAlphabet - parseInt(key);
         var newLetter = alphabet[newIndex];
