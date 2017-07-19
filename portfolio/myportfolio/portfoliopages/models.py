52c0b8f45efc4acba890e7d7faa0fa55e42fcd58
jastr945 <polina.jastr@gmail.com>
2017-07-19 10:54:01 -0700

    Made Media images show up in the template

--- portfolio/myportfolio/portfoliopages/models.py
+++ portfolio/myportfolio/portfoliopages/models.py
@@ -9,19 +9,19 @@ def profile_upload_handler(instance, filename):
 class Project(models.Model):
     name = models.CharField(max_length=255)
     img = models.ImageField(upload_to=profile_upload_handler)
-    description = models.CharField(max_length=500)
+    description = models.CharField(max_length=100)
     project_link = models.CharField(max_length=500)
 
     def __str__(self):
-        return '{} {} {} {}'.format(self.name, self.img, self.description, self.project_link)
+        return '{}'.format(self.name)
 
-    def save(self, *args, **kwargs):
-        if not self.pk:
-            super().save(*args, **kwargs)
-            path = self.img.path
-            image = Image.open(path)
-            width = self.img.width * .5
-            img = image.resize((int(width), int(width)))
-            img.save(path)
-        else:
-            super().save(*args, **kwargs)
+    # def save(self, *args, **kwargs):
+    #     if not self.pk:
+    #         super().save(*args, **kwargs)
+    #         path = self.img.path
+    #         image = Image.open(path)
+    #         width = self.img.width * .5
+    #         img = image.resize((int(width), int(width)))
+    #         img.save(path)
+    #     else:
+    #         super().save(*args, **kwargs)
