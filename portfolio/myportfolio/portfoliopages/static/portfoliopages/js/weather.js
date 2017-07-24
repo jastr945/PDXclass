86ab43234b7efa5bbaa77e58bb0e5c716a0e6ba4
jastr945 <polina.jastr@gmail.com>
2017-07-24 15:59:41 -0700

    Added Weather Forecast project

---
+++ portfolio/myportfolio/portfoliopages/static/portfoliopages/js/weather.js
@@ -0,0 +1,48 @@
+// Changes a background picture of a webpage and a weather icon depending on a weather code
+function insertPic (someid) {
+    if (someid >= 200 && someid < 300) {
+        $('#body').css({'background-image': 'url(/static/portfoliopages/img/thunderstorm.jpg)'});
+        $('#icon').html("<img src='/static/portfoliopages/img/weather_icon4.png'/>");
+    } else if (someid >= 300 && someid <= 531) {
+        $('#body').css({'background-image': 'url(/static/portfoliopages/img/rain.jpg)'});
+        $('#icon').html("<img src='/static/portfoliopages/img/weather_icon3.png'/>");
+    } else if (someid >= 600 && someid <= 622) {
+        $('#body').css({'background-image': 'url(/static/portfoliopages/img/snow.jpg)'});
+        $('#icon').html("<img src='/static/portfoliopages/img/weather_icon5.png'/>");
+    } else if (someid >= 701 && someid <= 781) {
+        $('#body').css({'background-image': 'url(/static/portfoliopages/img/fog.jpg)'});
+        $('#icon').html("<img src='/static/portfoliopages/img/weather_icon6.png'/>");
+    } else if (someid === '800') {
+        $('#body').css('background-image', 'url(/static/portfoliopages/img/sun.JPG)');
+        $('#icon').html("<img src='/static/portfoliopages/img/weather_icon1.png'/>");
+    } else if (someid >= 801 && someid <= 804) {
+        $('#body').css('background-image', 'url(/static/portfoliopages/img/clouds.jpg)');
+        $('#icon').html("<img src='/static/portfoliopages/img/weather_icon2.png'/>");
+    } else if (someid >= 900 && someid <= 962) {
+        $('#body').css('background-image', 'url(/static/portfoliopages/img/warning.jpg)');
+    }
+}
+
+//Gets weather info(API) and extracts necessary categories from the dictionary
+function getWeather () {
+    $.ajax({
+        url: "http://api.openweathermap.org/data/2.5/weather",
+        data: {
+            APPID: '6b919e5d511686a6a70d2728794a6fe5',
+            q: $('#city').val()
+        },
+        dataType: "jsonp",
+        type: "post",
+        success: function (data) {
+            $('#result').html('Weather: ' + data['weather'][0]['description'] + '<br/>' + 'Temperature: ' +
+                Math.round((data['main']['temp'] * 9/5 - 459.67)).toFixed(2) + 'F' + '<br/>' +
+                'Wind: ' + data['wind']['speed'] + 'm/s' + '<br/>' + 'Humidity: ' + data['main']['humidity']
+            + '%' + '<br/>' + 'Pressure: ' + data['main']['pressure'] + 'hpa');
+            var weatherId = data['weather'][0]['id'].toString();
+            insertPic(weatherId)
+            }
+        });
+}
+
+//loads the data as a text upon clicking the button
+$('#checkWeather').click(getWeather);
