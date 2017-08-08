6a886a9bb571575ad2676d20772d5dee68b9820c
jastr945 <polina.jastr@gmail.com>
2017-08-07 21:01:02 -0700

    Added confirm delete bar with working javascript

--- capstone/animalproject/animalapp/static/animalapp/js/add_animal.js
+++ capstone/animalproject/animalapp/static/animalapp/js/add_animal.js
@@ -2,16 +2,13 @@
  * Created by polina on 8/1/17.
  */
 
-// Opens the 'add animal' form upon clicking
+
+// Opens the 'add animal' form form upon clicking
 $('#addOption').click(function (e) {
     e.preventDefault();
     $('#addAnimal').toggle('fast')
 });
 
-$('#seeAllOption').click(function (e) {
-    e.preventDefault();
-    $('#tableContainer').slideToggle('fast')
-});
 
 // Depending on selection choice, shows either dog or cat form to fill out
 $(function() {
@@ -29,3 +26,27 @@ $(function() {
     });
 });
 
+
+// Opens the list of database entries upon clicking
+$('#seeAllOption').click(function (e) {
+    e.preventDefault();
+    $('#tableContainer').slideToggle('fast');
+});
+
+
+//Shows the bar with deletion choices
+$('.delete').each(function () {
+    $(this).click(function (e) {
+        e.preventDefault();
+        $(this).parent().parent().siblings('.deleteEntry').show();
+    });
+});
+
+
+//Hides the bar with deletion choices
+$('.noDelete').each(function () {
+    $(this).click(function (e) {
+        e.preventDefault();
+        $(this).parents('.deleteEntry').hide();
+    });
+});
