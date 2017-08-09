45922a2efe5c6de683b3cb616ad6009ec1276d87
jastr945 <polina.jastr@gmail.com>
2017-08-09 15:11:07 -0700

    Added a slideshow to animal profiles

---
+++ capstone/animalproject/animalapp/static/animalapp/js/animal_profile.js
@@ -0,0 +1,13 @@
+/**
+ * Created by polina on 8/9/17.
+ */
+
+'use strict';
+
+// enabling a slide show (PgwSlideshow JQuery plugin)
+$(document).ready(function() {
+    $('.pgwSlideshow').pgwSlideshow({
+      autoSlide: false
+    });
+});
+
