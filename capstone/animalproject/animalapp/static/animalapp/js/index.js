e190d8bb97833319e32af69a3929f5e5fcb4431a
jastr945 <polina.jastr@gmail.com>
2017-08-10 13:52:50 -0700

    Added styles for index page

--- capstone/animalproject/animalapp/static/animalapp/js/index.js
+++ capstone/animalproject/animalapp/static/animalapp/js/index.js
@@ -6,12 +6,12 @@
 'use strict';
 
 //removing placeholder from the search tab on focus and bringing it back on blur
-$('#searchField').focus(function(){
-   $(this).data('placeholder', $(this).attr('placeholder'))
-          .attr('placeholder','');
-}).blur(function(){
-   $(this).attr('placeholder', $(this).data('placeholder'));
-});
+// $('#searchField').focus(function(){
+//    $(this).data('placeholder', $(this).attr('placeholder'))
+//           .attr('placeholder','');
+// }).blur(function(){
+//    $(this).attr('placeholder', $(this).data('placeholder'));
+// });
 
 ;(function(window) {
 
