cc68a2aa573bc88d65d923b931d579d46182e6bb
jastr945 <polina.jastr@gmail.com>
2017-08-21 16:08:56 -0700

    Made a slug field optional, added a demo link field to the model

--- portfolio/myportfolio/portfoliopages/models.py
+++ portfolio/myportfolio/portfoliopages/models.py
@@ -11,9 +11,10 @@ class Project(models.Model):
     name = models.CharField(max_length=255, unique=True)
     img = models.ImageField(upload_to=profile_upload_handler)
     description = models.TextField(max_length=255)
+    demo_link = models.CharField(max_length=500, blank=True, null=True)
     project_link = models.CharField(max_length=500, blank=True, null=True)
     readme_link = models.CharField(max_length=500, blank=True, null=True)
-    slug = models.SlugField(max_length=50, default='')
+    slug = models.SlugField(max_length=50, blank=True, null=True)
 
     def __str__(self):
         return '{}'.format(self.name)
@@ -21,17 +22,6 @@ class Project(models.Model):
     def save(self, *args, **kwargs):
         self.slug = slugify(self.name)
         super(Project, self).save(*args, **kwargs)
-
-    # def save(self, *args, **kwargs):
-    #     if not self.pk:
-    #         super().save(*args, **kwargs)
-    #         path = self.img.path
-    #         image = Image.open(path)
-    #         width = self.img.width * .5
-    #         img = image.resize((int(width), int(width)))
-    #         img.save(path)
-    #     else:
-    #         super().save(*args, **kwargs)
 
 
 class Skill(models.Model):
