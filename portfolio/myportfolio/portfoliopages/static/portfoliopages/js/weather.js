187cc4ad4639532872185b4fa0ab6df61d94da74
jastr945 <polina.jastr@gmail.com>
2017-08-26 18:59:03 -0700

    Added flexbox

--- portfolio/myportfolio/portfoliopages/static/portfoliopages/js/weather.js
+++ portfolio/myportfolio/portfoliopages/static/portfoliopages/js/weather.js
@@ -1,25 +1,25 @@
 // Changes a background picture of a webpage and a weather icon depending on a weather code
 function insertPic (someid) {
     if (someid >= 200 && someid < 300) {
-        $('#body').css({'background-image': 'url(/static/portfoliopages/img/thunderstorm.jpg)'});
+        $('#mainContainer').css({'background-image': 'url(/static/portfoliopages/img/thunderstorm.jpg)'});
         $('#icon').html("<img src='/static/portfoliopages/img/weather_icon4.png'/>");
     } else if (someid >= 300 && someid <= 531) {
-        $('#body').css({'background-image': 'url(/static/portfoliopages/img/rain.jpg)'});
+        $('#mainContainer').css({'background-image': 'url(/static/portfoliopages/img/rain.jpg)'});
         $('#icon').html("<img src='/static/portfoliopages/img/weather_icon3.png'/>");
     } else if (someid >= 600 && someid <= 622) {
-        $('#body').css({'background-image': 'url(/static/portfoliopages/img/snow.jpg)'});
+        $('#mainContainer').css({'background-image': 'url(/static/portfoliopages/img/snow.jpg)'});
         $('#icon').html("<img src='/static/portfoliopages/img/weather_icon5.png'/>");
     } else if (someid >= 701 && someid <= 781) {
-        $('#body').css({'background-image': 'url(/static/portfoliopages/img/fog.jpg)'});
+        $('#mainContainer').css({'background-image': 'url(/static/portfoliopages/img/fog.jpg)'});
         $('#icon').html("<img src='/static/portfoliopages/img/weather_icon6.png'/>");
     } else if (someid === '800') {
-        $('#body').css('background-image', 'url(/static/portfoliopages/img/sun.JPG)');
+        $('#mainContainer').css('background-image', 'url(/static/portfoliopages/img/sun.JPG)');
         $('#icon').html("<img src='/static/portfoliopages/img/weather_icon1.png'/>");
     } else if (someid >= 801 && someid <= 804) {
-        $('#body').css('background-image', 'url(/static/portfoliopages/img/clouds.jpg)');
+        $('#mainContainer').css('background-image', 'url(/static/portfoliopages/img/clouds.jpg)');
         $('#icon').html("<img src='/static/portfoliopages/img/weather_icon2.png'/>");
     } else if (someid >= 900 && someid <= 962) {
-        $('#body').css('background-image', 'url(/static/portfoliopages/img/warning.jpg)');
+        $('#mainContainer').css('background-image', 'url(/static/portfoliopages/img/warning.jpg)');
     }
 }
 
@@ -38,6 +38,7 @@ function getWeather () {
                 Math.round((data['main']['temp'] * 9/5 - 459.67)).toFixed(2) + 'F' + '<br/>' +
                 'Wind: ' + data['wind']['speed'] + 'm/s' + '<br/>' + 'Humidity: ' + data['main']['humidity']
             + '%' + '<br/>' + 'Pressure: ' + data['main']['pressure'] + 'hpa');
+            $('#result').css('display', 'flex');
             var weatherId = data['weather'][0]['id'].toString();
             insertPic(weatherId)
             }
