9518a66c44a4bd9b36cefced5a1704ca60165807
jastr945 <polina.jastr@gmail.com>
2017-08-28 17:15:28 -0700

    Edit form shows up upon clicking the button

--- capstone/animalproject/animalapp/static/animalapp/js/add_animal.js
+++ capstone/animalproject/animalapp/static/animalapp/js/add_animal.js
@@ -5,57 +5,65 @@
 
 // Opens the 'add animal' form form upon clicking
 $('#addOption').click(function (e) {
-    e.preventDefault();
-    $('#addAnimalTable').toggle('fast')
+  e.preventDefault();
+  $('#addAnimalTable').toggle('fast')
 });
 
 
 // Depending on selection choice, shows either dog or cat form to fill out
 $(function() {
-    $('#id_species').change(function () {
-        if ($(this).val() === 'dog') {
-            $('.cat').hide();
-            $('.dog').show();
-        } else if ($('#id_species').val() === 'cat') {
-            $('.dog').hide();
-            $('.cat').show();
-        } else {
-            $('.dog').hide();
-            $('.cat').hide();
-        }
-    });
+  $('#id_species').change(function () {
+    if ($(this).val() === 'dog') {
+      $('.cat').hide();
+      $('.dog').show();
+    } else if ($('#id_species').val() === 'cat') {
+      $('.dog').hide();
+      $('.cat').show();
+    } else {
+      $('.dog').hide();
+      $('.cat').hide();
+    }
+  });
 });
 
 
 // Opens the list of database entries upon clicking
 $('#seeAllOption').click(function (e) {
-    e.preventDefault();
-    $('#tableContainer').slideToggle('fast');
+  e.preventDefault();
+  $('#tableContainer').slideToggle('fast');
 });
 
 
-//submits form on change of the drop-down filter lists dynamically
+// Submits form on change of the drop-down filter lists dynamically
 $(function() {
   $('#dbfilters').on('change', function (e) {
-      e.preventDefault();
-      $(this).submit()
+    e.preventDefault();
+    $(this).submit()
   })
 });
 
 
-//Shows the bar with deletion choices
+// Shows the bar with deletion choices
 $('.delete').each(function () {
-    $(this).click(function (e) {
-        e.preventDefault();
-        $(this).parent().parent().siblings('.deleteEntry').show();
-    });
+  $(this).click(function (e) {
+    e.preventDefault();
+    $(this).parent().parent().siblings('.deleteEntry').show();
+  });
 });
 
 
-//Hides the bar with deletion choices
+// Hides the bar with deletion choices
 $('.noDelete').each(function () {
-    $(this).click(function (e) {
-        e.preventDefault();
-        $(this).parents('.deleteEntry').hide();
-    });
+  $(this).click(function (e) {
+      e.preventDefault();
+      $(this).parents('.deleteEntry').hide();
+  });
+});
+
+// Shows the edit form upon clicking the specific edit button
+$('.edit').each(function () {
+  $(this).click(function (e) {
+    e.preventDefault();
+    $(this).parent().parent().parent().next('.editProfile').show();
+  });
 });
