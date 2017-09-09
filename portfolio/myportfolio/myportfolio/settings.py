4a7dadb4f6838a9ba970c8f9ed389d35ed927fd3
jastr945 <polina.jastr@gmail.com>
2017-09-08 20:53:33 -0700

    First deployment version

--- portfolio/myportfolio/myportfolio/settings.py
+++ portfolio/myportfolio/myportfolio/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'egswytb3%7wymhudk&2c7jcjan44$lvn80z6!&w4i!ad1uo&@3'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.0.106', 'polina.mee.how', 'localhost']
 
 
 # Application definition
