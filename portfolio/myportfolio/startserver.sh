031db1ec67b96ad49196ca7e59b0b2f0236234b8
jastr945 <polina.jastr@gmail.com>
2017-12-06 21:07:07 -0800

    Added fixtures and added loaddata to startserver

--- portfolio/myportfolio/startserver.sh
+++ portfolio/myportfolio/startserver.sh
@@ -1,2 +1,4 @@
+sleep 30
 python3 manage.py migrate
+python3 manage.py loaddata fixturefile.json
 uwsgi --chdir=./ --module=myportfolio.wsgi:application --env DJANGO_SETTINGS_MODULE=myportfolio.settings --socket=/tmp/myportfolio.sock --master --http :80
