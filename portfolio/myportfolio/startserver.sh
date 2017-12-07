67d9820ecb68176c2b8f4d4e06e6c45c4a1c64a5
jastr945 <polina.jastr@gmail.com>
2017-12-07 14:24:23 -0800

    Downgraded docker-compose, added healthcheck. Postgres now works

--- portfolio/myportfolio/startserver.sh
+++ portfolio/myportfolio/startserver.sh
@@ -1,4 +1,3 @@
-sleep 30
 python3 manage.py migrate
 python3 manage.py loaddata fixturefile.json
 uwsgi --chdir=./ --module=myportfolio.wsgi:application --env DJANGO_SETTINGS_MODULE=myportfolio.settings --socket=/tmp/myportfolio.sock --master --http :80
