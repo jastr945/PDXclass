0a989c61485fad8833d13f68bf8993a043298dff
jastr945 <polina.jastr@gmail.com>
2017-12-07 16:50:30 -0800

    Deployment-ready version

--- portfolio/myportfolio/startserver.sh
+++ portfolio/myportfolio/startserver.sh
@@ -1,3 +1,4 @@
 python3 manage.py migrate
+python3 manage.py collectmedia
 python3 manage.py loaddata fixturefile.json
 uwsgi --chdir=./ --module=myportfolio.wsgi:application --env DJANGO_SETTINGS_MODULE=myportfolio.settings --socket=/tmp/myportfolio.sock --master --http :80
